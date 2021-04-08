2412a586bd605c9d6fe96d581cbda764679d820b
HukuTa45 <nikitakuzmin20011@mail.ru>
2021-04-08 19:48:30 +0500

    fix v1.2

--- task4/Tree.cs
+++ task4/Tree.cs
@@ -1,4 +1,6 @@
-﻿namespace task4
+﻿using System.Data;
+
+namespace task4
 {
     public class Tree
     {
@@ -28,30 +30,20 @@
                 if (current.RightElement != null)
                     _AddTreeElement(current.RightElement, value);
                 else
-                    current.RightElement = new TreeElement
-                    {
-                        Value = value,
-                        Count = 1
-                    };
+                    current.RightElement = _AddNewElement(value);
             }
             else
             {
                 if (current.LeftElement != null)
                     _AddTreeElement(current.LeftElement, value);
                 else
-                    current.LeftElement = new TreeElement
-                    {
-                        Value = value,
-                        Count = 1
-                    };
+                    current.LeftElement = _AddNewElement(value);
             }
         }
 
         public bool FindElement(int value, out int outCount) 
             => _FindElement(_firstTreeElement, value, out outCount);
         
-
-
         private bool _FindElement(TreeElement current, int value, out int outCount)
         {
             if (current.Value == value)
@@ -71,6 +63,16 @@
 
             outCount = 0;
             return false;
+        }
+
+        private TreeElement _AddNewElement(int value)
+        {
+            var element = new TreeElement
+            {
+                Value = value,
+                Count = 1
+            };
+            return element;
         }
     }
 }
